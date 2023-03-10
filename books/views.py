a0159cde95af2f756121193a3b3f65146a647596
Jotape <jotaapedrolima@gmail.com>
2023-03-10 09:38:01 -0300

    Fix: Add Collaborator Permissions To Books

--- books/views.py
+++ books/views.py
@@ -1,12 +1,19 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializer import BookSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import Book
+from .permissions import IsCollaboratorOrNot
 
 class BookView(ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsCollaboratorOrNot]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    
 
 class BookDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer 
     lookup_url_kwarg = "book_id"
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsCollaboratorOrNot]
